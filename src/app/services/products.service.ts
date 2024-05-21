39d2e5389e679d59bf7622a193b31ba72a1526fc
spohie001 <z.sosinska001@gmail.com>
2024-05-21 11:58:36 +0200

    minor changes

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor() { }
+  constructor(private apiService: ApiService) { }
+  getProducts = (url: string, params: any): Observable<any> => {
+    return this.apiService.get(url, params);
+  }
 }
