b397fe6e35e52405d2a7d7d9f9ae1e9d976dcead
spohie001 <z.sosinska001@gmail.com>
2024-05-21 13:09:37 +0200

    created needed interfaces

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from './api.service';
 import { Observable } from 'rxjs';
+import { PaginationParams, Products } from '../../types';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,10 @@ import { Observable } from 'rxjs';
 export class ProductsService {
 
   constructor(private apiService: ApiService) { }
-  getProducts = (url: string, params: any): Observable<any> => {
-    return this.apiService.get(url, params);
+  getProducts = (url: string, params: PaginationParams): Observable<Products> => {
+    return this.apiService.get(url, {
+      params,
+      responseType: 'json'
+    });
   }
 }
