6a454114a549a95e8b5d4843d33c6647ccf60411
spohie001 <z.sosinska001@gmail.com>
2024-05-21 15:29:19 +0200

    getting products from backend works

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../services/products.service';
+import { Products } from '../../types';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,17 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  constructor(
+    private productsService: ProductsService
+  ){}
 
+  ngOnInit(){
+    this.productsService
+    .getProducts('http://localhost:3000/clothes',
+    {page: 0, perPage:5})
+    .subscribe((products: Products) => {
+      console.log(products.items);
+    })
+  }
 }
