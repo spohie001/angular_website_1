2f6d10404346656c4e6e067faf9283e7a0588a33
spohie001 <z.sosinska001@gmail.com>
2024-05-22 11:19:57 +0200

    product are visible on body

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,25 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductsService } from '../services/products.service';
-import { Products } from '../../types';
+import { Product, Products } from '../../types';
+import { ProductComponent } from "../components/product/product.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss',
+    imports: [ProductComponent, CommonModule]
 })
 export class HomeComponent implements OnInit{
   constructor(
     private productsService: ProductsService
   ){}
 
+  products: Product[] = [];
   ngOnInit(){
     this.productsService
     .getProducts('http://localhost:3000/clothes',
     {page: 0, perPage:5})
     .subscribe((products: Products) => {
       console.log(products.items);
+      this.products = products.items;
     })
   }
 }
